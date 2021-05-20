500c2de7b4831d45528a935351f328422b78a055
vinhqbui <bui.quoc.vinh@hotmail.com>
2021-05-20 10:18:24 -0700

    Update data and Building Heat Map

--- Building_Heat_Map.py
+++ Building_Heat_Map.py
@@ -51,11 +51,9 @@ def WordNetLemma(arrayList):
 #-----Import data-----#
 train = pd.read_excel('./Data/training_set_rel3_set1.xlsx')
 test = pd.read_excel('./Data/valid_set_set1.xlsx')
-train.set_index('ID')
-test.set_index('essay_id')
 y_train = train['Score']
 y_test = test['Score']
-X = pd.concat([train,test])
+X = pd.concat([train,test], ignore_index = True)
 
 train_numberOfSentences = X['Essay Content'].apply(lambda x: len(x.split('.')))
 train_numberOfWords = X['Essay Content'].apply(lambda x: len(x.split()))
@@ -67,24 +65,23 @@ content = content.apply(lambda x: WordNetLemma(word_tokenize(x)))
 content = content.apply(lambda x: RemoveStopWords(word_tokenize(x)))
 #content = content.apply(lambda x: SnowballStemmer(word_tokenize(x)))
 
-sns.set_theme()
+#sns.set_theme()
 heatmapData = list()
-for neighbors in range(3, 11, 1):
+for dimensions in range(80, 351, 10):
     accuracyOfDimension = list()
-    for dimensions in range(80, 351, 10):
-        svd = TruncatedSVD(n_components=dimensions)
-        tfidf = TfidfVectorizer(min_df = 0.01, max_df=0.85, stop_words='english')
-        
-        x_transform = tfidf.fit_transform(content)
-        x_transform = sparse.hstack((x_transform, train_numberOfSentences[:,None]))
-        x_transform = sparse.hstack((x_transform, train_numberOfWords[:,None]))
-        
-        #x_transform = SVD_for_S.SVD(x_transform.toarray(), dimensions)
-        x_transform = svd.fit_transform(x_transform)
-        
-        x_train = x_transform[:len(train)]
-        x_test = x_transform[len(train):]
-        
+    svd = TruncatedSVD(n_components=dimensions)
+    tfidf = TfidfVectorizer(min_df = 0.01, max_df=0.85, stop_words='english')
+    
+    x_transform = tfidf.fit_transform(content)
+    x_transform = sparse.hstack((x_transform, train_numberOfSentences[:,None]))
+    x_transform = sparse.hstack((x_transform, train_numberOfWords[:,None]))
+    
+    #x_transform = SVD_for_S.SVD(x_transform.toarray(), dimensions)
+    x_transform = svd.fit_transform(x_transform)
+    
+    x_train = x_transform[:len(train)]
+    x_test = x_transform[len(train):]
+    for neighbors in range(3, 11, 1):     
         nearestNeighbors = NearestNeighbors(n_neighbors=neighbors)
         nearestNeighbors.fit(x_train)
         test_dist, test_ind = nearestNeighbors.kneighbors(x_test)
@@ -104,6 +101,6 @@ for neighbors in range(3, 11, 1):
         accuracyOfDimension.append(accuracy)
     heatmapData.append(accuracyOfDimension)
     
-df = pd.DataFrame(heatmapData, columns=list(range(80, 351, 10)), index=list(range(3,11,1)))
+df = pd.DataFrame(heatmapData, columns=list(range(3,11,1)), index=list(range(80, 351, 10)))
 plt.figure(figsize=(16, 16))
 sns.heatmap(df)
